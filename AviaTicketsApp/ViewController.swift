4c9ecbfc383f3596d6700de971887659634a2995
KristelWhite <KristelWhite@users.noreply.github.com>
2024-05-30 15:31:53 +0500

    Initial Commit

---
+++ AviaTicketsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AviaTicketsApp
+//
+//  Created by Кристина Пастухова on 30.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
